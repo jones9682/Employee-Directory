b0fd9d53f5eb0a1ca7cc6565af767838c68b2d30
Bobby Jones Jr <jones9682@yahoo.com>
2020-10-06 23:45:23 -0400

    added headings

---
+++ src/components/Headings/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Headings() {
+    return (
+        <thead>
+            <tr>
+                <th >IMAGE</th>
+                <th >FIRST-NAME</th>
+                <th >LAST-NAME</th>
+                <th >PHONE</th>
+                <th >EMAIL</th>
+                <th >DOB</th>
+            </tr>
+        </thead>
+    )
+}
+
+export default Headings;
