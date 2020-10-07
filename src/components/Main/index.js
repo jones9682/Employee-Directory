1de05f30532edeb636a7655f9bfda91501e21a98
Bobby Jones Jr <jones9682@yahoo.com>
2020-10-06 23:46:21 -0400

    added main component with searchform

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import SearchForm from "../SearchForm/index.js";
+
+function Main() {
+    return (
+        <>
+            <SearchForm />
+        </>
+    );
+};
+
+export default Main;
