78fea403a827f76e5e305b0c763e3d909e6ad966
Bobby Jones Jr <jones9682@yahoo.com>
2020-10-06 23:45:04 -0400

    added header

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            {/* not functional yet */}
+            {/* <p>Click on each heading to filter or use the search box to narrow your results</p> */}
+        </div>
+    )
+}
+
+export default Header;
