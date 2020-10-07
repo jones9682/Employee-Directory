ab2e799d80a6f44049b3c0d51a6e08207732f9a1
Bobby Jones Jr <jones9682@yahoo.com>
2020-10-06 23:43:49 -0400

    added api function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import "./App.css";
+import API from "./utils/API";
+import Wrapper from './components/Wrapper';
+import Main from './components/Main';
+import Header from './components/Header';
+import Headings from './components/Headings';
+import TableBody from './components/TableBody';
+
+
+
+class App extends Component {
+  state = {
+    employees: [],
+  };
+  componentDidMount() {
+    API.getUsers()
+      .then(res => {
+        console.log(res.data.results)
+        this.setState({ employees: res.data.results })
+      })
+      .catch(err => console.log(err));
+  }
+  handleInputChange = event => {
+    this.setState({ search: event.target.value });
+  };
+  render() {
+    return (
+      <div className="App">
+
+        <Wrapper>
+          <Header />
+          <Main />
+          <Headings />
+          <TableBody employees={this.state.employees} />
+        </Wrapper>
+
+      </div>
+    );
+  }
 }
 
 export default App;
